94342f7a58f0b8b2bf3f0c29e7298a1a290c46ed
Curtis Long <curtisplong@gmail.com>
2019-01-11 10:12:54 -0500

    add new augmented datasets

---
+++ lib/ConvModel2.py
@@ -0,0 +1,120 @@
+import os
+import sys
+import glob
+import time
+import imageio
+import itertools
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.misc import imresize 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.class_weight import compute_class_weight 
+
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Conv1D, Dropout
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD, RMSprop
+from keras.callbacks import ModelCheckpoint
+
+from lib import Dataset 
+from lib import utils
+
+
+class ConvModel(object):
+    convolutions = [32, 64]
+    pooling = [0, 2]
+    mlp = [100]
+    name = "AnonymousModel"
+    epochs = 30
+    optimizer = 'adam'
+    activation = 'relu'
+    dropout = 0.25
+
+    def __init__(self, data, convolutions = [32, 64], pooling = [0,2], mlp = [100], batch = 8, name = 'default', epochs = 30, optimizer = 'adam', activation = 'relu', dropout = 0.25):
+        self.ds = data 
+        self.convolutions = convolutions
+        self.mlp = mlp
+        self.batch_size = batch
+        self.name = name
+        self.epochs = epochs 
+        self.optimizer = optimizer
+        self.activation = activation
+        self.dropout = dropout 
+        self.pooling = pooling
+
+        # for backwards compat
+        self.train_generator = self.ds.train_generator
+        self.validation_generator = self.ds.validation_generator
+        self.test_generator = self.ds.test_generator
+
+        self.build()
+
+
+    def build(self):
+        # Model Building
+        self.model = Sequential()
+        for i,j in enumerate(self.convolutions):
+            self.model.add(Conv2D(j, activation=self.activation, kernel_size=3,
+                         input_shape=(self.ds.img_height, self.ds.img_width, 3), padding='same', kernel_initializer='TruncatedNormal', bias_initializer='zeros'))
+            # if pooling specified for this layer
+            if self.pooling[i]:
+                self.model.add(MaxPooling2D(pool_size=(self.pooling[i],self.pooling[i])))
+
+        self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        for y in self.mlp:
+            self.model.add(Dense(y, activation=self.activation))
+
+        self.model.add(Dropout(self.dropout))
+        self.model.add(Dense(4, activation='softmax'))
+
+        weightsfile = self.name + '-best.hdf5'
+        if(os.path.isfile(weightsfile)):
+            print("Loading weights...")
+            self.model.load_weights(weightsfile)
+
+        #opt = RMSprop(lr=0.01)
+        self.model.compile(optimizer=self.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        print(self.model.summary())
+
+
+    def train(self):
+        self.checkpoint = ModelCheckpoint(filepath = os.path.join("weights", self.name + "-best.hdf5"), monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+        self.hist = self.model.fit_generator(
+            self.ds.train_generator, 
+            steps_per_epoch=len(self.ds.train_generator) // self.batch_size,
+            validation_data=self.ds.validation_generator,
+            validation_steps=len(self.ds.validation_generator) // self.batch_size,
+            epochs=self.epochs,
+            callbacks = [self.checkpoint]
+        )
+
+        #Plot Testing and Validation MSE and Loss
+        utils.plot_val_loss(self.hist.history, filename=os.path.join("results", self.name + "-val_loss.png"))
+        utils.plot_val_acc(self.hist.history, filename=os.path.join("results", self.name + "-val_acc.png"))
+
+
+    def test(self):
+        y_pred = self.model.predict_generator(self.ds.test_generator, steps = len(self.ds.X_test))
+        #y_pred = self.model.predict(self.ds.X_test)
+        # Compute confusion matrix
+        cnf_matrix = confusion_matrix(self.ds.y_test.argmax(axis=1), y_pred.argmax(axis=1))
+        np.set_printoptions(precision=2)
+
+        # Plot non-normalized confusion matrix
+        utils.plot_confusion_matrix(cnf_matrix, classes=self.ds.categories,
+                              title='Confusion matrix, without normalization')
+        plt.savefig(os.path.join("results", self.name + "-confusion.png"))
+        plt.close()
+
+        #self.model.save_weights('curtis300.h5')
+
+
+#############################################################################################
+
+
---
+++ lib/Dataset2.py
@@ -0,0 +1,120 @@
+import os
+import sys
+import glob
+import time
+import imageio
+import itertools
+import argparse
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.misc import imresize 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.class_weight import compute_class_weight 
+
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Conv1D, Dropout
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD, RMSprop
+from keras.callbacks import ModelCheckpoint
+
+
+class Dataset2(object):
+    imagedir = "dataset-master/JPEGImages/"
+    labelsfile = "dataset-master/labels.csv"
+    img_width = 80
+    img_height = 60
+    categories = []
+    labels = pd.DataFrame()
+    batch_size = 8
+
+    def __init__(self, img_width=160, img_height=120, batch_size=8):
+        self.labels = pd.read_csv(self.labelsfile)
+        self.labels = self.labels.loc[:, ['Image', 'Category']]
+        self.labels['Image'] = pd.to_numeric(self.labels['Image'])
+        self.labels = self.labels.dropna()
+        self.labels = self.labels[~self.labels.Category.str.contains(",")]
+        self.labels = self.labels[~self.labels.Category.str.contains("BASOPHIL")]
+        self.class_weight = compute_class_weight('balanced', np.unique(self.labels['Category']), self.labels['Category']) 
+        self.labels = pd.get_dummies(self.labels, columns=['Category'])
+        self.batch_size = batch_size
+        #print(class_weight)
+
+        self.labels['filename'] = self.labels.Image.apply(lambda x: self.imagedir + "BloodImage_" + str(x).zfill(5) + ".jpg")
+        # Filter for only image files that actually exist
+        self.labels = self.labels[self.labels['filename'].apply(lambda x: os.path.isfile(x))]
+
+        # Load and resize self.images
+        self.images = []
+        for x in self.labels['filename']:
+            img = imageio.imread(x, as_gray = False)
+            img = imresize(img, (self.img_height,self.img_width))
+            self.images.append(img)
+
+        self.images = np.array(self.images)
+        print(self.images.shape)
+        #print(self.images[0])
+
+        #plt.imshow(self.images[0])
+        #plt.show()
+
+        # Reduce to only categories
+        self.categories = ['Category_EOSINOPHIL', 'Category_LYMPHOCYTE', 'Category_MONOCYTE', 'Category_NEUTROPHIL']
+        self.y = self.labels.loc[:,self.categories].values
+
+        #print(str(images.shape) + ', ' + str(y.shape))
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.images, self.y, test_size=0.2, stratify=self.y, random_state=159)
+        #print(str(X_split.shape) + ', ' + str(y_split.shape))
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.y_train, test_size=0.25, stratify=self.y_train, random_state=159)
+
+        self.train_gen = ImageDataGenerator(rescale = 1. / 255,
+                                            horizontal_flip=True, 
+                                            rotation_range=10, 
+                                            featurewise_center=False,
+                                            samplewise_center=False,  
+                                            featurewise_std_normalization=False,
+                                            samplewise_std_normalization=False)
+
+        self.train_gen.fit(self.X_train)
+
+        self.val_gen = ImageDataGenerator(rescale = 1. / 255 )
+        self.val_gen.fit(self.X_val)
+
+        self.test_gen = ImageDataGenerator(rescale = 1. / 255 )
+        self.test_gen.fit(self.X_test)
+
+        self.train_generator = self.train_gen.flow_from_directory(os.path.join("dataset2-master", "images", "TRAIN"), target_size=(self.img_height, self.img_width), batch_size=self.batch_size)
+        self.validation_generator = self.val_gen.flow_from_directory(os.path.join("dataset2-master", "images", "TEST"), target_size=(self.img_height, self.img_width), batch_size=self.batch_size)
+        self.test_generator = self.val_gen.flow_from_directory(os.path.join("dataset2-master", "images", "TEST"), target_size=(self.img_height, self.img_width), batch_size=self.batch_size)
+        #self.test_generator = self.test_gen.flow_from_directory(os.path.join("dataset2-master", "images", "TEST_SIMPLE"), target_size=(self.img_width, self.img_height), batch_size=self.batch_size)
+        self.test_generator = self.test_gen.flow(np.array(self.X_test), self.y_test, batch_size=1)
+
+
+    def stats(self):
+        # Category_BASOPHIL  Category_EOSINOPHIL  Category_LYMPHOCYTE  Category_MONOCYTE  Category_NEUTROPHIL
+        #basophil = self.labels['Category_BASOPHIL'].sum()
+        eosinophil = self.labels['Category_EOSINOPHIL'].sum()
+        lymphocyte = self.labels['Category_LYMPHOCYTE'].sum()
+        monocyte = self.labels['Category_MONOCYTE'].sum()
+        neutrophil = self.labels['Category_NEUTROPHIL'].sum()
+        count = eosinophil + lymphocyte + monocyte + neutrophil 
+        print(self.labels.describe())
+        #Basophil   {bas}
+        print("""
+        Eosinophil {eos}
+        Lymphocyte {lym}
+        Neutrophil {neu}
+        Monocyte   {mono}
+        """.format(
+            #bas=basophil, 
+            eos=eosinophil, lym=lymphocyte, neu=neutrophil,
+                   mono=monocyte))
+
+        #train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+        print("Train:      " + str(self.X_train.shape) + ', ' + str(self.y_train.shape))
+        print("Validation: " + str(self.X_val.shape) + ', ' + str(self.y_val.shape))
+        print("Test:       " + str(self.X_test.shape) + ', ' + str(self.y_test.shape))
+
